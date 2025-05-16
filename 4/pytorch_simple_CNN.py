05633a21d86b9e3a337d4cc09a9dd6c9d25776e3
Shah Jawad Islam <s.jawad@hawarit.com>
2025-05-16 18:02:25 +0600

    completed CNN

--- 4/pytorch_simple_CNN.py
+++ 4/pytorch_simple_CNN.py
@@ -23,7 +23,7 @@ class NN(nn.Module):
 
 # TODO: Create simple CNN
 class CNN(nn.Module):
-    def __init__(self, input_channels=1, num_classes=100):
+    def __init__(self, input_channels=1, num_classes=10):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(
             in_channels=1,
@@ -32,6 +32,30 @@ class CNN(nn.Module):
             stride=(1, 1),
             padding=(1, 1),
         )
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.conv2 = nn.Conv2d(
+            in_channels=8,
+            out_channels=16,
+            kernel_size=(3, 3),
+            stride=(1, 1),
+            padding=(1, 1),
+        )
+        self.fc1 = nn.Linear(16 * 7 * 7, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        return x
+
+
+model = CNN()
+x = torch.randn((64, 1, 28, 28))
+# print(x.shape)
+print(model(x).shape)
 
 
 # model = NN(784, 10)
@@ -42,11 +66,11 @@ class CNN(nn.Module):
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Hyperparameters
-input_size = 784
+in_channels = 1
 num_classes = 10
 learing_rate = 0.001
 batch_size = 64
-num_epochs = 1
+num_epochs = 5
 
 # Load Data
 train_dataset = datasets.MNIST(
@@ -60,7 +84,8 @@ test_dataset = datasets.MNIST(
 test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True)
 
 # Initialize Network
-model = NN(input_size=input_size, num_classes=num_classes).to(device)
+# model = NN(input_size=input_size, num_classes=num_classes).to(device)
+model = CNN().to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -74,8 +99,8 @@ for epoch in range(num_epochs):
         data = data.to(device=device)
         targets = targets.to(device=device)
 
-        # Get to correct shape
-        data = data.reshape(data.shape[0], -1)
+        # # Get to correct shape
+        # data = data.reshape(data.shape[0], -1)
 
         # Forward
         scores = model(data)
@@ -105,7 +130,7 @@ def check_accuracy(loader, model):
         for x, y in loader:
             x = x.to(device=device)
             y = y.to(device=device)
-            x = x.reshape(x.shape[0], -1)
+            # x = x.reshape(x.shape[0], -1)
 
             scores = model(x)
             _, predictions = scores.max(1)
